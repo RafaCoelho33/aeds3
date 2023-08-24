8b1e5fd02dfd0362c740b92cdd81a27c1462e960
RafaCoelho33 <rafael.coelhomendonca@gmail.com>
2023-08-24 09:28:12 -0300

    Commit

--- Arquivos_fonte/models/NbaPlayer.java
+++ Arquivos_fonte/models/NbaPlayer.java
@@ -7,63 +7,18 @@ public class NbaPlayer {
     protected String stats;
     protected long insertion_date;
     protected float player_height;
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getPlayer_name() {
-        return player_name;
-    }
-
-    public void setPlayer_name(String player_name) {
-        this.player_name = player_name;
-    }
-
-    public String getTeam_abbreviation() {
-        return team_abbreviation;
-    }
-
-    public void setTeam_abbreviation(String team_abbreviation) {
-        this.team_abbreviation = team_abbreviation;
-    }
-
-    public String getstats() {
-        return stats;
-    }
-
-    public void setstats(String stats) {
-        this.stats = stats;
-    }
-
-    public long getInsertion_date() {
-        return insertion_date;
-    }
-
-    public void setInsertion_date(long insertion_date) {
-        this.insertion_date = insertion_date;
-    }
-
-    public float getPlayer_height() {
-        return player_height;
-    }
-
-    public void setPlayer_height(float player_height) {
-        this.player_height = player_height;
-    }
+    protected int size;
+    
 
     public NbaPlayer(int id, String player_name, String team_abbreviation, String stats, long insertion_date,
-            float player_height) {
+            float player_height, int size) {
         this.id = id;
         this.player_name = player_name;
         this.team_abbreviation = team_abbreviation;
         this.stats = stats;
         this.insertion_date = insertion_date;
         this.player_height = player_height;
+        this.size = size;
     }
 
     public NbaPlayer(String line) {
@@ -73,11 +28,12 @@ public class NbaPlayer {
         this.player_name = info[1];
         this.team_abbreviation = info[2];
         this.player_height = Float.parseFloat(info[4]);
-        this.stats = this.setStats(info[12], info[13], info[14]); // 12,13,14
+        this.stats = this.setStats(info[12], info[13], info[14]);   // 12,13,14
+        this.size = this.setSize();                                  
 
     }
 
-    public byte[] toByteArray() throws Exception {  //cria o array de bytes com os dados
+    public byte[] toByteArray() throws Exception { // cria o array de bytes com os dados
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         DataOutputStream dos = new DataOutputStream(baos);
 
@@ -94,10 +50,17 @@ public class NbaPlayer {
 
     private String setStats(String pts, String reb, String ast) {
         String stats = pts + "," + reb + "," + ast;
-        
 
         return stats;
     }
+
+    private int setSize(){
+        int size = 0;
+
+        
+
+        return size;
+    }   
 
     public void fromByteArray(byte[] array) throws Exception { // recebe o array de dados e instancia o objeto
 
