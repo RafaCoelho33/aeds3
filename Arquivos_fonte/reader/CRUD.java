23107cab7fa376f7cd88c8b3ad081696a273d7eb
thiagoxz5 <thiago22004@hotmail.com>
2023-08-28 08:34:25 -0300

    voce é puta rafael

--- Arquivos_fonte/reader/CRUD.java
+++ Arquivos_fonte/reader/CRUD.java
@@ -1,5 +1,69 @@
 package reader;
 
+import java.io.FileNotFoundException;
+import java.io.RandomAccessFile;
+
+import models.NbaPlayer;
+
 public class CRUD {
     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// --------------- CREATE ---------------
+
+public static boolean crate (NbaPlayer Nba) throws FileNotFoundException{
+    RandomAccessFile raf = new RandomAccessFile("./Database/player_db.db", "rw");
+    try {
+        //processo de escrita do novo id 
+        raf.seek(0);
+        int ultimoID = raf.readInt();
+        int novoID = ultimoID + 1;
+        Nba.setId(novoID);
+        raf.seek(0);
+        raf.writeInt(novoID);
+
+        //criação de um novo registro
+        raf.seek(raf.length());
+        raf.writeByte(0);
+        raf.writeInt(Nba.toByteArray().length); // escreve o tamanho do registro
+
+        //uma ideia que eu nao fiz, ao inves de inserir 1 por 1 cria um que ja insere todos os registros de uma vez (não sei fazer kkk)
+
+        raf.writeInt(Nba.getID()); // escreve o id no array de bytes
+        raf.writeUTF(Nba.getName()); // escreve o nome no array de bytes
+        raf.writeLong(Nba.getDate()); // escreve a date no array de bytes
+        raf.writeUTF(Nba.getAbbreviation()); // escreve o abbreviation no array de bytes 
+        raf.writeFloat(Nba.getHeight()); // escreve a height no array de bytes
+        System.out.println(Nba);
+        raf.close();
+        return true;
+    } catch(Exception e) {
+        System.out.println("-> Erro ao criar o registro!");
+        return false;
+    }
+}
+
+// --------------- READS ---------------
+
+
+
+
+
 }
