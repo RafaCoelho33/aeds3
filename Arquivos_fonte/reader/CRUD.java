fc353cce64b3a31be7d6101bffd9d6c3a208984f
RafaCoelho33 <rafael.coelhomendonca@gmail.com>
2023-08-30 11:12:35 -0300

    Commit

--- Arquivos_fonte/reader/CRUD.java
+++ Arquivos_fonte/reader/CRUD.java
@@ -22,40 +22,28 @@ public class CRUD {
 
     // --------------- CREATE ---------------
 
-    public boolean create(NbaPlayer Nba) throws FileNotFoundException {
+    public void create(NbaPlayer player) throws FileNotFoundException {
         RandomAccessFile raf = new RandomAccessFile("./Database/player_db.db", "rw");
         try {
-            // processo de escrita do novo id
+            // id creation
             raf.seek(0);
-            int ultimoID = raf.readInt();
-            int novoID = ultimoID + 1;
-            Nba.setId(novoID);
+            int lastId = raf.readInt();
             raf.seek(0);
-            raf.writeInt(novoID);
+            player.setId(lastId + 1);
+            raf.seek(0);
+            raf.writeInt(player.getId());
 
-            // criação de um novo registro
+            // writing the new player
+            byte[] array = player.toByteArray();
             raf.seek(raf.length());
-            raf.writeUTF(";"); // ! trocar a lapide pra ;
-            raf.writeInt(Nba.toByteArray().length); // escreve o tamanho do registro
+            raf.writeChar(lapide);
+            raf.writeInt(array.length);
+            raf.write(array);
 
-            // uma ideia que eu nao fiz, ao inves de inserir 1 por 1 cria um que ja insere
-            // todos os registros de uma vez (não sei fazer kkk)
-
-            raf.writeInt(Nba.getId()); // escreve o id no array de bytes
-            raf.writeUTF(Nba.getPlayer_name()); // escreve o nome no array de bytes
-            raf.writeLong(Nba.getInsertion_date()); // escreve a date no array de bytes
-            raf.writeUTF(Nba.getTeam_abbreviation()); // escreve o abbreviation no array de bytes
-            raf.writeUTF(Nba.getStats());
-            raf.writeFloat(Nba.getPlayer_height()); // escreve a height no array de bytes
-            System.out.println(Nba);
-            raf.close();
-            return true;
         } catch (Exception e) {
             System.out.println("-> Erro ao criar o registro! -> " + e);
-            return false;
         }
+
+        // --------------- READS ---------------
     }
-
-    // --------------- READS ---------------
-
 }
