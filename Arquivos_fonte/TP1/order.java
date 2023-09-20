099093c1a74f548fae7a62e91e60dd4b9f5ae5b2
RafaCoelho33 <rafael.coelhomendonca@gmail.com>
2023-09-20 16:30:13 -0300

    Interleaving

---
+++ Arquivos_fonte/TP1/order.java
@@ -0,0 +1,46 @@
+package TP1;
+
+//TODO criar nova pasta raiz para a ordenação
+import java.io.*;
+
+import models.NbaPlayer;
+import reader.CRUD;
+
+public class order {
+    private int m; // number of registros
+    private int n; // number of paths
+    private static final String file_path = "../Database/player_db.db";
+
+    public order() {
+
+    }
+
+    public order(int m, int n) {
+        this.m = m;
+        this.n = n;
+    }
+
+    public void interlacaçãoComum() throws Exception{
+
+        RandomAccessFile rafInput = new RandomAccessFile(file_path, "r");
+        RandomAccessFile rafOutput;
+
+        File[] files = new File[n];
+        for(int i = 0; i < n; i++){files[i] = new File("../Database/inputFile"+ i);}
+
+        int numRegister = 0;
+        rafInput.seek(0);
+
+        for(int j = 0; j < files.length; j++){
+            rafOutput = new RandomAccessFile(files[j], "rw");
+
+            for(int i = 0; rafInput.getFilePointer() < rafInput.length() && numRegister <= m; i++){            
+                //realizar a leitura do arquivo database e jogar pro arquivo temporario
+
+            }
+    }
+
+    rafInput.close();
+}
+
+}
